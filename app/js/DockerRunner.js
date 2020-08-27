96add7c004a28f88c76cb91130255a6d5d02efa0
Marcel Fabian Krüger <zauguin@gmail.com>
2020-08-27 18:05:54 +0200

    Pull missing images
    This of course assumes that you restrict the allowed images properly.
    Similar to `docker create`, if never updates the images once pulled.

--- app/js/DockerRunner.js
+++ app/js/DockerRunner.js
@@ -420,7 +420,21 @@ module.exports = DockerRunner = {
 
     const createAndStartContainer = () =>
       dockerode.createContainer(options, function (error, container) {
-        if (error != null) {
+        if ((error != null ? error.statusCode : undefined) === 404) {
+          return dockerode.pull(options.Image, function (error, image) {
+            if (error != null) {
+              return callback(error)
+            }
+            image.resume()
+            image.on("end", () =>
+              dockerode.createContainer(options, function (error, container) {
+                if (error != null) {
+                  return callback(error)
+                }
+                return startExistingContainer()
+              }))
+          })
+        } else if (error != null) {
           return callback(error)
         }
         return startExistingContainer()
